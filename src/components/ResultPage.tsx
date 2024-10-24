515ef6da59ec2e892ff1521c1bb5ef409695597a
MrSeager <seager1993@gmail.com>
2024-10-24 11:31:49 +0300

    10/24/2024

---
+++ src/components/ResultPage.tsx
@@ -0,0 +1,19 @@
+import React, { FC } from 'react';
+//Components
+import './resultPage.css';
+//Bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import { Container } from 'react-bootstrap';
+//Animation
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+const ResultPage: FC = () => {
+    return (
+        <Container fluid>
+            hello
+        </Container>
+    );
+}
+
+export default ResultPage;
