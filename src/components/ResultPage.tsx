9f8755111d4cdd4b20bd93a454395d401276089e
MrSeager <seager1993@gmail.com>
2024-10-24 11:47:21 +0300

    update

--- src/components/ResultPage.tsx
+++ src/components/ResultPage.tsx
@@ -1,17 +1,34 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 //Components
 import './resultPage.css';
 //Bootstrap
 import 'bootstrap/dist/css/bootstrap.css';
-import { Container } from 'react-bootstrap';
+import { Container, Image } from 'react-bootstrap';
 //Animation
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+//Axios
+import axios from 'axios';
+
+interface DataLinesProps {
+    category: string,
+    score: number,
+    icon: string,
+}
 
 const ResultPage: FC = () => {
+    const [dataProps, setDataProps] = useState<DataLinesProps[]>([]);
+
+    useEffect(() => {
+        axios.get('https://raw.githubusercontent.com/MrSeager/results-summary/refs/heads/main/src/data.json').then((response) => {
+            setDataProps(response.data);
+        });
+      }, []);
+
     return (
         <Container fluid>
-            hello
+            <h1>{dataProps[1].icon}</h1>
+            <Image src={dataProps[1].icon} />
         </Container>
     );
 }
